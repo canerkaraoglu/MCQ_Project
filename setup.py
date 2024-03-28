daddee41133ae2d961b65dd24fb22e3140a9f342
Caner Karaoglu <caner_karaoglu@hotmail.com>
2024-03-28 01:41:12 +0100

    File update

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 """
-Installing local packages into local environment.
+Installs local packages into local environment.
 """
+
+from setuptools import find_packages, setup
+
+setup(
+    name = "mcq_project",
+    version = "0.0.1",
+    author = "Caner Karaoglu",
+    author_email = "caner_karaoglu@hotmail.com",
+    install_requires = ["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages()
+)
